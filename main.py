5203bc07cb7be30c38f5426fbd1e6671bf3e2af4
unknown <marimo3458@gmail.com>
2021-02-28 09:04:06 +0900

    changed url

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ import webbrowser
 #You know the codes and so do i
 
 if __name__ == "__main__":
-    webbrowser.open("https://youtu.be/dQw4w9WgXcQ?autoplay=1")
+    webbrowser.open("https://youtu.be/dkWjvAc0jLQ?autoplay=1")
